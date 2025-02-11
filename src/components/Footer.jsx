2b1530d0ac80e00f2f88dd6e6c1cd340cba9941a
tomygl1234 <tomasgimenezlascano123@gmail.com>
2025-02-11 20:43:39 +0100

    Footer change commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div className='bg-gray-900 p-6 text-white'>
-            <div className='flex flex-col w-9/12 m-auto'>
-                <h6 className='underline'>Contact info:</h6>
-                <p className=''>Miguel Wils</p>
-                <p>Tel: +34 654814450</p>
-                <p>Email: michelwils2016@gmail.com</p>
+        <div className='bg-gray-900 p-6 text-white text-center'>
+            <div className='border p-4'>
+                <div className='inline-block p-6'>
+                    <h6 className='underline text-2xl mb-4'>Contact info:</h6>
+                    <p>Tel: <span className='text-amber-400'>+34 654814450</span></p>
+                    <p>E-mail: <span className='text-amber-400'>michelwils2016@gmail.com</span></p>
+                </div>
+                <div>
+                    <p className='font-bold font-serif text-md uppercase border border-white p-3 inline-block mb-6'>Miguel Wils</p>
+                    <p>&copy; 2025 Miguel Wils. All rights reserved.</p>
+                </div>
             </div>
-            <div className='text-center'>
-                <p>&copy; 2025 Miguel Wils. All rights reserved.</p>
-            </div>
+
         </div>
     )
 }
